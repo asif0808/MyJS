54c4ed43f0dab0725869d914d050d255d99dbc1a
Aasif Ansari <116907759+asif0808@users.noreply.github.com>
2024-12-24 07:50:10 +0000

    container config done

---
+++ Js_Basics/datatypes.js
@@ -0,0 +1,11 @@
+//alert(3+3) not working coz we are using nodejs not brower
+// see documentation in mdn or tc39
+//watch out for differences in undefined and null
+const name="john" 
+let marks=45
+let isPass=false
+let Promote;
+console.log(typeof name)
+console.log(typeof marks)
+console.log(typeof isPass)
+console.log(typeof Promote)
---
+++ Js_Basics/variables.js
@@ -0,0 +1,21 @@
+// there arethree types of variables in js
+// const,let,var
+const id=2023
+let subject="english"
+var marks=45 //not preferred
+console.log(id)
+console.log(subject)
+console.log(marks)
+// or
+console.table([id,subject,marks])
+// id=2024 //(not allowed)
+subject="math"
+marks=55
+console.table([id,subject,marks])
+city="Hyderabad" //not recommended  
+console.table([id,subject,marks,city])
+let state; //undefined for now
+console.table([id,subject,marks,state])
+
+
+
